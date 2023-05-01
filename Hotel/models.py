a14a59d9724a152e85a814ef876f85706fcd07f0
ujunwa <ujmarylee@gmail.com>
2023-05-01 15:18:40 -0700

    initial

--- Hotel/models.py
+++ Hotel/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+CATEGORY_CHOICES = (
+    ("Rooms & Apartments", "Rooms & Apartments"),
+    ("Food & Restaurant", "Food & Restaurant"),
+    ("Spa & Fitness", "Spa & Fitness"),
+    ("Sports & Gaming", "Sports & Gaming"),
+    ("Event & Party", "Events & Party"),
+    ("GYM & Yoga", "GYM & Yoga")
+)
+
+class Products(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to="Products_images")
+
+
+    class Meta:
+        verbose_name_plural = "Products"
+
+
+    def __str__(self):
+        return self.name
+    
+
+
+class Category(models.Model):
+    name = models.CharField(choices=CATEGORY_CHOICES, max_length=100)
+    description = models.TextField()
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+
+    def __str__(self):
+        return self.name
--- Hotel/views.py
+++ Hotel/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 
 
 def home_page(request):
-    return render(request, "Hotel/index.html")
+    products = Products.objects.all()
+    context = {
+        "products" : products
+    }
+
+
+    return render(request, "Hotel/index.html", context)
